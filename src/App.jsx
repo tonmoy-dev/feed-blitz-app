b38d478c6ec519ceda074d9436030c75d61d686d
tonmoy-dev <rxdevs100@gmail.com>
2024-03-04 23:52:20 +0600

    basic routing added with react router dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import Registration from "./pages/Registration";
 
 function App() {
-  return <h1>FeedBlitz App</h1>;
+  return (
+    <Routes>
+      <Route element={<Home />} path="/" exact />
+      <Route element={<Login />} path="/login" />
+      <Route element={<Registration />} path="/register" />
+      <Route element={<Profile />} path="/profile" />
+      <Route element={<NotFound />} path="*" />
+    </Routes>
+  );
 }
 
 export default App;
