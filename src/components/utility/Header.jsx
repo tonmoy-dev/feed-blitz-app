e9b5432c56c6972f6ef40899530c33898b75a434
tonmoy-dev <rxdevs100@gmail.com>
2024-03-06 12:06:07 +0600

    header section updated with actual data from server

--- src/components/utility/Header.jsx
+++ src/components/utility/Header.jsx
@@ -4,10 +4,19 @@ import NotificationIcon from "../../assets/icons/notification.svg";
 import Avatar from "../../assets/images/avatars/avatar.jpg";
 import Logo from "../../assets/images/logo.png";
 import { useAuth } from "../../hooks/useAuth";
+import useProfile from "../../hooks/useProfile";
 import Logout from "../auth/Logout";
+
+const baseUrl = import.meta.env.VITE_SERVER_BASE_URL; // base url
 
 export default function Header() {
   const { auth } = useAuth();
+  const { state } = useProfile();
+
+  // If a user visits profile page, then the user data will be provided by state from profile Context. Otherwise, user data will come from auth Context.
+  const user = state?.user ?? auth?.user;
+
+  const fullName = `${user?.firstName} ${user?.lastName}`;
 
   return (
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
@@ -35,13 +44,11 @@ export default function Header() {
           <Logout />
 
           <button className="flex-center !ml-8 gap-3">
-            <span className="text-lg font-medium lg:text-xl">
-              {auth?.user?.firstName}
-            </span>
+            <span className="text-lg font-medium lg:text-xl">{fullName}</span>
             <Link to="/profile">
               <img
                 className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px] rounded-full"
-                src={Avatar}
+                src={user ? `${baseUrl}/${user?.avatar}` : Avatar}
                 alt="Avatar"
               />
             </Link>
